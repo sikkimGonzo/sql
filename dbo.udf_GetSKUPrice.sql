662498eeea9ff8103a047a75509dbef3294f1d74
sikkimGonzo <99316262+sikkimGonzo@users.noreply.github.com>
2023-01-17 16:46:18 +0500

    Update dbo.udf_GetSKUPrice.sql

--- dbo.udf_GetSKUPrice.sql
+++ dbo.udf_GetSKUPrice.sql
@@ -2,11 +2,12 @@ create function dbo.udf_GetSKUPrice(@ID_SKU as int)
 returns decimal(18, 2)
   begin
     declare @val decimal;
-    select @val = Value from dbo.Basket as b
-                  where b.ID_SKU = @ID_SKU;
     declare @qua decimal;
-    select @qua = Quantity from dbo.Basket as b
-                  where b.ID_SKU = @ID_SKU;
+    select 
+      	@val = sum(Value),
+        @qua = sum(Quantity)
+    from dbo.Basket as b
+	  where b.ID_SKU = @ID_SKU;
     return @val / @qua
   end
 go
