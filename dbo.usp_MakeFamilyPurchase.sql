9b8388d3f8edf355555de7be01feb28b877478ea
sikkimGonzo <99316262+sikkimGonzo@users.noreply.github.com>
2023-01-17 16:53:52 +0500

    Update dbo.usp_MakeFamilyPurchase.sql

--- dbo.usp_MakeFamilyPurchase.sql
+++ dbo.usp_MakeFamilyPurchase.sql
@@ -1,12 +1,19 @@
-create procedure dbo.usp_MakeFamilyPurchase 
+create procedure dbo.usp_MakeFamilyPurchase
   @FamilySurName varchar(255)
 as
-  if exists(select SurName from dbo.Family where SurName = @FamilySurName)
-      begin
-          update dbo.Family
-          set BudgetValue = BudgetValue - 1 -- Family и Basket не связаны
-          where SurName = @FamilySurName
-      end;
-  else
-      select 'SurName not found'
+begin
+  if not exists(select SurName from dbo.Family where SurName = @FamilySurName)
+    throw 51000, 'The record does not exist.', 1;
+  begin try
+    update dbo.Family
+    set BudgetValue = BudgetValue - (select sum(Value) from dbo.Basket)
+    where SurName = @FamilySurName
+  end try
+  begin catch
+    select 
+      error_number() as errNum,
+      error_message() as errMess,
+      error_state() as errState
+  end catch
+end
 go
